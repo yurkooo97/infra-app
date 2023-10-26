61bc07ac4fac27c682cc14e04a708ec979823452
Yura Tykhan <43648928+yurkooo97@users.noreply.github.com>
2023-10-26 23:03:03 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -31,12 +31,12 @@ variable "health_check" {
 
 variable "rds_username" {
   description = "enter the rds_username"
-  default     = "root"
+  default     = ""
   type        = string
 }
 
 variable "rds_password" {
   description = "enter the rds_password"
-  default     = "password"
+  default     = ""
   type        = string
 }
